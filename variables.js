767ac33bfee8b4215b17b4072103805d3a0cb991
Imaad Uddin <imaaduddin@icloud.com>
2021-03-20 12:20:28 -0500

    variables

---
+++ variables.js
@@ -0,0 +1,14 @@
+let age = 21;
+let firstName = "Obito";
+let lastName = "Uchiha";
+let online = true;
+let randomVariable;
+age = age + 1;
+const pi = 3.14159
+
+randomVariable = "Sharingan";
+
+console.log(randomVariable);
+console.log(firstName + " " + lastName);
+console.log("My age is " + age);
+console.log(pi);

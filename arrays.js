ce72fcb8bd6aa60a8ea704a357184efe3c93ca8e
Imaad Uddin <imaaduddin@icloud.com>
2021-03-22 22:57:02 -0500

    arrays

---
+++ arrays.js
@@ -0,0 +1,25 @@
+// array = a special variable that can hold more than one value 
+// each space is known as an element 
+// you can access elements in an array be referring to the index number ex.[0]
+
+let cars = ["Mercedes", "Audi", "BMW"];
+
+// console.log(cars[0]);
+
+// cars.push("Porche");
+// console.log(cars);
+
+// cars.pop();
+// console.log(cars);
+
+// let numOFCars = cars.length;
+// console.log(numOFCars);
+
+// cars.sort();
+// console.log(cars);
+
+// cars.reverse();
+// console.log(cars);
+
+let lastCar = cars[cars.length - 1];
+console.log(lastCar);

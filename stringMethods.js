e74cf94832d6a0df3560777dad57243b8913bb6e
Imaad Uddin <imaaduddin@icloud.com>
2021-03-26 23:20:44 -0500

    string methods

---
+++ stringMethods.js
@@ -0,0 +1,9 @@
+// useful string properties and methods 
+
+let myName = "Itachi Uchiha";
+
+console.log(myName.length);
+console.log(myName.charAt(0));
+console.log(myName.indexOf(" "));
+console.log(myName.lastIndexOf("i"));
+console.log();

8ce280d229a0bc137f24a85fd24fe53ccb3e4d50
Imaad Uddin <imaaduddin@icloud.com>
2021-04-06 15:35:14 -0500

    strict equality

---
+++ strictEquality.js
@@ -0,0 +1,14 @@
+// loose equality operator ==
+// strict equality operator ===
+
+// checks for equal value AND type 
+// compares if value is equal & if the data type is equal 
+
+let pi = 3.14; // number 
+// let pi = "3.14"; // string 
+
+if (pi === 3.14) {
+  console.log("Yes that is pi!");
+} else {
+  console.log("Not pi!");
+}

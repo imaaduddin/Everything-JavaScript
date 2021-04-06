b34ba28b23fe0fa17f8f73152fae6209ec7217e5
Imaad Uddin <imaaduddin@icloud.com>
2021-04-06 15:09:28 -0500

    if statements

---
+++ ifStatements.js
@@ -0,0 +1,13 @@
+// if statement = a basic form of decision making
+// if a condition is true, then do something 
+// if not, then don't do it
+
+let age = 18;
+
+if (age === 23) {
+  console.log("Hello");
+} else if (age === 18) {
+  console.log("Yo");
+} else {
+  console.log("Bye");
+}

2ac99668b0835bb4a8cb25b0d0c3bb3510ea0dbe
Imaad Uddin <imaaduddin@icloud.com>
2021-04-08 17:04:02 -0500

    for loops

---
+++ forLoop.js
@@ -0,0 +1,11 @@
+// for loop = repeat a loop a limited amount of times for (declare index; condition; step)
+
+// for (i = 0; i <= 10; i++) {
+//   console.log(i);
+// }
+
+for (i = 10; i > 0; i--) {
+  console.log(i);
+}
+
+console.log("Happy New Year!!");

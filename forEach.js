cbaad6a1e053e9140b8fe8769f61e091837c1172
Imaad Uddin <imaaduddin@icloud.com>
2021-04-15 20:52:50 -0500

    for each

---
+++ forEach.js
@@ -0,0 +1,12 @@
+// forEach() = performs a function for each element in an array 
+
+let total = 0;
+let cart = [5, 6, 7, 8, 9];
+
+function checkOut(element) {
+  total += element;
+}
+
+cart.forEach(checkOut);
+
+console.log("Your total is: $" + total);

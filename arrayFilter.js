7fe3466258622759cef848265244a2bb17905f55
Imaad Uddin <imaaduddin@icloud.com>
2021-04-15 21:05:02 -0500

    filter

---
+++ arrayFilter.js
@@ -0,0 +1,13 @@
+// filter() = Rreturns the elements of an array that meets a condition specified in a function 
+
+let students = [16, 17, 18, 19, 20];
+
+function checkAge(age, index, array) {
+  if (age >= 18) {
+    return age;
+  }
+}
+
+let adultStudents = students.filter(checkAge);
+
+console.log(adultStudents);

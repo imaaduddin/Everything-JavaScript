978f3c51d9aa177328fec5f8ecb65a99b32d85db
Imaad Uddin <imaaduddin@icloud.com>
2021-04-15 20:32:28 -0500

    hoisting

---
+++ hoisting.js
@@ -0,0 +1,6 @@
+// hoisting = JS's default behavior of moving declerations, to the top of the current scope.
+// A variable can be used before it has been declared. Beneficial to understand to help avoid bugs
+
+// good practice to declare all variables at the top of every scope 
+
+
